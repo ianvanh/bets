3e2e22325324069a47103c89d23f78c1d10f37d2
ianvanh <publishort1522@gmail.com>
2025-05-20 00:30:22 -0500

    add dominio

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   info: {
     name_page: 'Newbets',
     desc: 'Newbets, la mejor forma de ganarle al sistema.',
-    dominio: ''
+    dominio: 'https://bets-1ltd.onrender.com'
   },
   isProduction: process.env.NODE_ENV === 'production',
   github: {
