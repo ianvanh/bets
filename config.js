6c4d2642aa8c1de6407fb642e8241c95f88d5200
ianvanh <publishort1522@gmail.com>
2025-05-21 10:19:22 -0500

    delete config production

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ module.exports = {
     desc: 'Newbets, la mejor forma de ganarle al sistema.',
     dominio: 'https://bets-1ltd.onrender.com'
   },
-  isProduction: process.env.NODE_ENV === 'production',
   github: {
     token: process.env.GITHUB_TOKEN
   }
